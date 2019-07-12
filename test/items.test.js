7d10ed0ab48df1182334656516a36cc3ea5dae31
Claire Cassidy <clacassidy@gmail.com>
2019-07-11 17:51:36 -0700

    fixed package errors, first test and item route

--- test/items.test.js
+++ test/items.test.js
@@ -1,10 +1,18 @@
 const request = require('supertest')
-
+const app = require('../lib.app');
 
 
 describe('express routes', () => {
   it('POSTS an object', () => {
     return request(app)
-    
-  })
-})
+    .post('/api/v1/items');
+    .send({ glitter: 'biodegradable', adhesive: 'superglue', deployment: 'cannon-pressureized' })
+    .then(res => {
+      expect(res.body).toEqual({
+        glitter: 'biodegradable',
+        adhesive: 'superglue',
+        deployment: 'cannon-pressureized'
+      });
+    });
+  });
+});
